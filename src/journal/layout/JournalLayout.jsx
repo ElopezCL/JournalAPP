813eacc172685b63b0e64a60d1c7fd1edde8b456
ElopezCL <em.lopezcortes@gmail.com>
2024-06-18 18:30:31 +0100

    Avance 18-06 - 13:30 practica

---
+++ src/journal/layout/JournalLayout.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export const JournalLayout = ({children}) => {
+  return (
+    <Box sx={{display:"flex"}}>
+       
+       {/*Navbar*/}
+
+         {/*Sidebar*/}
+    <Box component="main"
+        sx={{flexGrow:1, p3}}
+    >
+
+    </Box>
+    </Box>
+  )
+}
